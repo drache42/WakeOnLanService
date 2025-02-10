ea753d9549728eb1faaa28a2b45afe32ff314110
Steffen Froehlich <drache42@gmail.com>
2025-02-10 08:50:09 -0800

    Working multi stage docker image

--- build.ps1
+++ build.ps1
@@ -32,10 +32,32 @@ param (
     [switch]$Run
 )
 
-# Read the configuration file
-$config = Get-Content -Path "debug.json" | ConvertFrom-Json
-$url = $config.URL
-$macAddress = $config.MAC_ADDRESS
+# Function to read .env file and set environment variables
+function Set-EnvVarsFromDotEnv {
+    param (
+        [string]$envFilePath
+    )
+
+    if (Test-Path $envFilePath) {
+        $envFileContent = Get-Content $envFilePath
+        foreach ($line in $envFileContent) {
+            if ($line -match '^\s*#') { continue }  # Skip comments
+            if ($line -match '^\s*$') { continue }  # Skip empty lines
+            if ($line -match '^\s*([^=]+)\s*=\s*(.*)\s*$') {
+                $name = $matches[1].Trim()
+                $value = $matches[2].Trim()
+                [System.Environment]::SetEnvironmentVariable($name, $value)
+                Write-Output "Set environment variable: $name=$value"
+            }
+        }
+    } else {
+        Write-Output "The .env file does not exist at path: $envFilePath"
+        exit 1
+    }
+}
+
+# Read the .env file and set environment variables
+Set-EnvVarsFromDotEnv -envFilePath ".env"
 
 # Build the Docker image
 Write-Output "Building the Docker image..."
@@ -50,7 +72,7 @@ if ($BuildOnly) {
 # If the --run switch is set, run the Docker container after building the image
 if ($Run) {
     Write-Output "Running the Docker container..."
-    docker run -it --rm -p 4200:4200 -e URL=$url -e MAC_ADDRESS=$macAddress drache42/wakeonlanservice:latest
+    docker run -it --rm -p 4200:4200 -e URL=$env:URL -e MAC_ADDRESS=$env:MAC_ADDRESS drache42/wakeonlanservice:latest
     exit 0
 }
 
